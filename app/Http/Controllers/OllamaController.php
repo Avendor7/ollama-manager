f4f187d68b4d076e985ad9d258fce0be75081b43
StephenW <steve.wiggins7@gmail.com>
2025-04-10 22:44:09 -0400

    different package for ollama

--- app/Http/Controllers/OllamaController.php
+++ app/Http/Controllers/OllamaController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Cloudstudio\Ollama\Facades\Ollama;
+use ArdaGnsrn\Ollama\Ollama;
 use Inertia\Inertia;
 
 class OllamaController extends Controller
 {
     public function index()
     {
-        $response = Ollama::models()->get();
+        $client = Ollama::client();
+
+        $response = $client->models()->list();
+
         \Log::debug(json_encode($response));
         return Inertia::render('Dashboard');
     }
