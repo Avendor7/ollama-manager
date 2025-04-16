2fa7ca258cb0d98f81cfe6ccf627135e7bab845a
StephenW <steve.wiggins7@gmail.com>
2025-04-15 21:40:49 -0400

    Status page

--- resources/js/pages/Status.vue
+++ resources/js/pages/Status.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-  defineProps<{}>()
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Status',
+        href: '/status',
+    },
+];
 </script>
 
 <template>
-    <h1>Status</h1>
+    <Head title="Status" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
+                <PlaceholderPattern />
+                <div class="absolute inset-0 flex items-center justify-center">
+                    <h1 class="text-2xl font-bold">Status Page</h1>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
 </template>
