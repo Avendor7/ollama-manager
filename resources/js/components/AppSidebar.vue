a8595bb971cdae6d8dfc37b73541f37f7c0f65a7
StephenW <steve.wiggins7@gmail.com>
2025-06-23 21:56:28 -0400

    Remove `NavFooter` component usage and associated `footerNavItems` in `AppSidebar` component.

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
-import NavFooter from '@/components/NavFooter.vue';
 import NavUser from '@/components/NavUser.vue';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { type NavItem } from '@/types';
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '@/components/ui/sidebar';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 import ChatSessions from '@/components/ChatSessions.vue';
 
-const footerNavItems: NavItem[] = [
-    {
-        title: 'Github Repo',
-        href: 'https://github.com/stephen6611/ollama-manager',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://ollama.com/docs',
-        icon: BookOpen,
-    },
-];
 </script>
 
 <template>
@@ -41,7 +34,6 @@ const footerNavItems: NavItem[] = [
         </SidebarContent>
 
         <SidebarFooter>
-            <NavFooter :items="footerNavItems" />
             <NavUser />
         </SidebarFooter>
     </Sidebar>
