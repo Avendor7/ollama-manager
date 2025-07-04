61b1131ab2b06bc44d165304b6ccb08364e52bbe
StephenW <steve.wiggins7@gmail.com>
2025-07-03 22:33:11 -0400

    Style `UnloadModel` button in `AppSidebarHeader` with rounded edges, background color, and hover effects.

--- resources/js/components/AppSidebarHeader.vue
+++ resources/js/components/AppSidebarHeader.vue
@@ -32,6 +32,6 @@ function unloadModel(): void {
         <span>
             {{runningList.models?.[0]?.name}}
         </span>
-        <Button @click="unloadModel()">UnloadModel</Button>
+        <Button class="rounded-lg bg-blue-500 px-4 py-2 font-semibold text-white shadow transition hover:bg-blue-600 disabled:opacity-60 cursor-pointer" @click="unloadModel()">UnloadModel</Button>
     </header>
 </template>
